ce2248c9458178129ecc15beb11d1f2ea14b52ef
Kirill Biller <biller.k@yandex.ru>
2025-05-11 23:17:04 +0300

    Update pagination methods to return nilable integers

--- app/modules/api/pagination.rb
+++ app/modules/api/pagination.rb
@@ -12,19 +12,19 @@ module Api
     sig { returns(Integer) }
     def first_page = 1
 
-    sig { returns(Integer) }
+    sig { returns(T.nilable(Integer)) }
     def current_page = @response["X-Page"].presence&.to_i
 
-    sig { returns(Integer) }
+    sig { returns(T.nilable(Integer)) }
     def next_page = @response["X-Next-Page"].presence&.to_i
 
-    sig { returns(Integer) }
+    sig { returns(T.nilable(Integer)) }
     def last_page = @response["X-Total-Pages"].presence&.to_i
 
-    sig { returns(Integer) }
+    sig { returns(T.nilable(Integer)) }
     def total_pages = @response["X-Total-Pages"].presence&.to_i
 
-    sig { returns(Integer) }
+    sig { returns(T.nilable(Integer)) }
     def prev_page = @response["X-Prev-Page"].presence&.to_i
   end
 end
